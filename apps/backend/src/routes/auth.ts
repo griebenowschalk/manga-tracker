1618c8fcb6715a669a16c2e6adedfb4e4274d176
Schalk Griebenow <schalkgriebenow@Scalk-Griebenow.local>
2025-08-12 15:35:57 +0200

    Type for router

--- apps/backend/src/routes/auth.ts
+++ apps/backend/src/routes/auth.ts
@@ -3,7 +3,7 @@ import { register } from '../controllers/auth';
 import { validate } from '../middleware/validation.middleware';
 import { registerSchema } from '../validations/auth.validation';
 
-const router = Router();
+const router: Router = Router();
 
 router.post('/register', validate(registerSchema), register);
 
